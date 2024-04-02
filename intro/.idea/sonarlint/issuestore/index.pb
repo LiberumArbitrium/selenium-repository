76dd549d2abdc0d060efa7bd554369e9412a3d90
LiberumArbitrium <beresneva_n96@mail.ru>
2024-04-02 21:45:16 +0600

    Задание 1

---
+++ intro/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/test/java/ru/stqa/maven/AppTest.java,b\e\bed0fbb0c233b9d1ce9fa98d4d00ea7c26b0893e
+Z
+*src/test/java/ru/stqa/maven/FirstTest.java,e\d\ed60e1972947f681336e83553126e2fed9921be4
---
+++ intro/src/test/java/ru/stqa/maven/FirstTest.java
@@ -0,0 +1,35 @@
+package ru.stqa.maven;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void FirstTest()
+    {
+       driver.get("https://software-testing.ru/");
+    }
+
+
+    @After
+    public void stop()
+    {
+        driver.quit();
+        driver = null;
+    }
+
+
+}
