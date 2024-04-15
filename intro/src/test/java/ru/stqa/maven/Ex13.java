3d0defa3267ebbdc3d0708e63fa4206a467ac9b6
LiberumArbitrium <beresneva_n96@mail.ru>
2024-04-16 00:37:19 +0600

    add ex13

--- intro/src/test/java/ru/stqa/maven/Ex13.java
+++ intro/src/test/java/ru/stqa/maven/Ex13.java
@@ -8,10 +8,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 public class Ex13
@@ -22,7 +27,7 @@ public class Ex13
 
     @Before
     public void start(){
-        driver = new ChromeDriver(); //new FirefoxDriver();
+        driver = new ChromeDriver(); //new FirefoxDriver(); new ChromeDriver();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
@@ -30,12 +35,40 @@ public class Ex13
     @Test
     public void checkItem ()
     {
-
+     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
      driver.get("http://localhost/litecart/en/");
 
+        for (int i = 0; i < 3; i++) {
 
+            driver.findElement(By.xpath("//li[contains (@class,'product')]")).click();
 
+            if (!driver.findElements(By.name("options[Size]")).isEmpty())
+            {
+                driver.findElement(By.name("options[Size]")).click();
+                driver.findElement(By.xpath("//option[@value = 'Small']")).click();
+            }
+            driver.findElement(By.name("add_cart_product")).click();
 
+            WebElement cart = driver.findElement(By.className("quantity"));
+            String num = String.valueOf(i+1);
+            String test= cart.getText();
+            wait.until(ExpectedConditions.textToBePresentInElement(cart,num));
+
+            driver.findElement(By.linkText("Home")).click();
+
+        }
+
+        driver.findElement(By.xpath("//div[@id='cart']")).click();
+
+        ArrayList <WebElement> countRow = new ArrayList<>(driver.findElements(By.xpath("//td[@class = 'item']")));
+        while (countRow.size()>0)
+        {
+            wait.until(ExpectedConditions.elementToBeClickable(By.name("remove_cart_item"))).click();
+            wait.until(ExpectedConditions.numberOfElementsToBeLessThan(By.xpath("//td[@class = 'item']"),countRow.size()));
+            countRow = new ArrayList<>(driver.findElements(By.xpath("//td[@class = 'item']")));
+        }
+
+       // wait.until(ExpectedConditions.stalenessOf(driver.findElements(By.xpath("//td[@class = 'item']"));
 
     }
 
